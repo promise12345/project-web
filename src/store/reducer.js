d024061f791cf949ea610fcd88766da8d45bf2ee
promise12345 <1197483717@qq.com>
2021-05-12 13:14:29 +0800

    store

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,13 @@
+// redux-actions框架提供了两个常用的API函数
+// createAction() 创建actions
+// handleActions() 代替reducer中switch或if else
 import initState from './state'
+import { ADD_TODO, BLUS_TODO } from './actionTypes'
 
 export const getNum = (state=initState.num, action) => {
   switch(action.type){
-    case 'ADD': return ++state;
-    case 'BLUS': return --state;
+    case ADD_TODO: return ++state;
+    case BLUS_TODO: return --state;
     default: return state
   }
 }
